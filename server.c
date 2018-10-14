e4ce36a38230452d517fdf800633d70af021ee56
jua15377 <email_id>
2018-10-14 03:42:26 -0600

    accept ip and port from command line

--- server.c
+++ server.c
@@ -1,32 +1,49 @@
 #include <stdio.h>
-#include <sys/socket.h> //For Sockets
+#include <sys/socket.h> 
 #include <stdlib.h>
-#include <netinet/in.h> //For the AF_INET (Address Family)
+#include <netinet/in.h> 
 #include <string.h>
 
-struct sockaddr_in serv; //This is our main socket variable.
-int fd; //This is the socket file descriptor that will be used to identify the socket
+struct sockaddr_in serv; //variable principal de los socket
+int fd; //socket file descriptor idnetifica al socket
 int conn; //This is the connection file descriptor that will be used to distinguish client connections.
 char message[100] = ""; //This array will store the messages that are sent by the server
+u_short port;
 
+void * start_server(){
+	// preparando conexion
+	printf("Preparando servidor...\n");
+	serv.sin_family = AF_INET; //protocolo de red
+	serv.sin_port = htons(port); //puesto a escuchar
+	serv.sin_addr.s_addr = INADDR_ANY;//ip locar
+
+	// crea un nuevo socket
+	fd = socket(AF_INET, SOCK_STREAM, 0); //This will create a new socket and also return the identifier of the socket into fd.
+	// TODO: handle errors, you can add an if condition that checks whether fd is greater than 0. If it isn't, prompt an error
+	// asigna ip a socket
+	bind(fd, (struct sockaddr *)&serv, sizeof(serv)); 
+}
 
 int main(int argc, char const *argv[]){
-	serv.sin_family = AF_INET;
-	serv.sin_port = htons(8096); //Define the port at which the server will listen for connections.
-	serv.sin_addr.s_addr = INADDR_ANY;
+	if (argc < 2 | argc > 2 ){
+		printf("Error: no se indico el numero de puerto\n");
+		exit(0);
+	}
+	else{
+		port = (u_short) atoi(argv[1]);
+	}
+	
+	start_server();
 
-	fd = socket(AF_INET, SOCK_STREAM, 0); //This will create a new socket and also return the identifier of the socket into fd.
+	// escucha conexiones, segundo atributo maximo de conexiones
+	printf("espreando conexion\n");
+	listen(fd,5); 
 
-	// To handle errors, you can add an if condition that checks whether fd is greater than 0. If it isn't, prompt an error
-
-	bind(fd, (struct sockaddr *)&serv, sizeof(serv)); //assigns the address specified by serv to the socket
-	listen(fd,5); //Listen for client connections. Maximum 5 connections will be permitted.
-
-	//Now we start handling the connections.
-
+	// manejando conexiones
 	while(conn = accept(fd, (struct sockaddr *)NULL, NULL)) {
-    	int pid;
-    	if((pid = fork()) == 0) {
+    	pid_t pid;
+    	pid = fork();
+    	if(pid == 0) {
 	        while (recv(conn, message, 100, 0)>0) {
 	            printf("Message Received: %s\n", message);
 	            //An extra breaking condition can be added here (to terminate the child process)            
--- test_client.c
+++ test_client.c
@@ -8,15 +8,31 @@ struct sockaddr_in server; //This is our main socket variable.
 int fd; //This is the socket file descriptor that will be used to identify the socket
 int conn; //This is the connection file descriptor that will be used to distinguish client connections.
 char message[100] = ""; //This array will store the messages that are sent by the server
-
+char *server_IP;
+u_short port;
 
 int main(int argc, char const *argv[]){
+	// manejo de ingreso de variables
+	if (argc < 2){
+		printf("Error: no se indico la ip y el numero de puerto\n");
+		exit(0);
+	}
+	else if(argc > 3){
+		printf("Demasiados argumentos\n");
+		exit(0);
+	}
+	else{
+		server_IP = argv[1];
+		port = (u_short) atoi(argv[2]);
+	}
+
+
 	fd = socket(AF_INET, SOCK_STREAM, 0);
 
 	server.sin_family = AF_INET;
-	server.sin_port = htons(8096);
+	server.sin_port = htons(port);
 
-	inet_pton(AF_INET, "127.0.0.1", &server.sin_addr); //This binds the client to localhost
+	inet_pton(AF_INET, server_IP, &server.sin_addr); //This binds the client to localhost
 
 	connect(fd, (struct sockaddr *)&server, sizeof(server)); //This connects the client to the server.
 
