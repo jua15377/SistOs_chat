78e4c88a2e920cc764e3b79438bc248ee55da617
jua15377 <email_id>
2018-10-16 12:28:31 -0600

    multithread comunication

--- server.c
+++ server.c
@@ -3,32 +3,34 @@
 #include <stdlib.h>
 #include <netinet/in.h> 
 #include <string.h>
+#include "pthread.h"
 
 #define MAX_USER 10
 #define BUFFER_MSJ_SIZE 1024
+#define CLIENT_ADDRESS_LENGTH 100
 
 struct sockaddr_in serv, cl_socket; //variable principal del socket del server
 int fd, cl_socket_fd; //socket file descriptor idnetifica al socket
 int conn; //This is the connection file descriptor that will be used to distinguish client connections.
 char message[100] = ""; //This array will store the messages that are sent by the server
 u_short port;
-
+// a variable to see how many clients are connected
 int clients_count = 0;
-
+char client_address[CLIENT_ADDRESS_LENGTH];
 
 // store data from clients
 struct cliente
 {	
-	struct sockaddr_in socket;
-	int fd;
-	int connfd;
-	int enable;
-	char alias[50];
-	int status;
-	int uid;
+	struct sockaddr_in socket;// the clien socjet
+	int fd; // file descriptor from the above socket
+	int connfd; //the file descriptor generated from the new connection
+	int enable; // the status of the client
+	char alias[50]; // the alias/nickname asseigned to by the client
+	int status;  // this shold save the state of the client
+	int uid;	// an Id to identify each client
 };
 
-struct cliente connected_clients[MAX_USER];
+struct cliente connected_clients[MAX_USER]; // this array will store all the connected client
 
 /* SE CREA EL SERVIDOR */
 
@@ -45,13 +47,42 @@ void * start_server(){
 	bind(fd, (struct sockaddr *)&serv, sizeof(serv)); 
 }
 
-void * send_message(int new_socket_fd, struct sockaddr *cl_addr) {
+
+/*THIS FUNCIONT SEND A MESSAGE USING THE FD FROM TEH CONNETTION ANT THE CLIENT ADDR
+* NEEDS MORE WORK
+*/
+void * send_message(int new_socket_fd, struct sockaddr *cl_addr, void * message) {
 	sendto(new_socket_fd, message, 100, 0, (struct sockaddr *) &cl_addr, sizeof cl_addr);
+}
+
+/*Recibe message*/
+void * recive(void * socket) {
+    int socket_fd, response;
+    char message[BUFFER_MSJ_SIZE];
+    memset(message, 0, BUFFER_MSJ_SIZE); // Clear message buffer
+    socket_fd = (int) socket;
+
+    // Print received message
+    while(1) {
+        response = recvfrom(socket_fd, message, BUFFER_MSJ_SIZE, 0, NULL, NULL);
+        if (response) {
+            printf("%s", message);
+        	int i = 0;
+            for ( i = 0; i < MAX_USER; ++i)
+            {
+            	send_message(connected_clients[i].connfd, &connected_clients[i].socket, message);
+            	printf("conn dentro de for %d\n",connected_clients[i].connfd);
+            }
+        }
+    }
 }
 
 /* FUNCION MAIN*/
 int main(int argc, char const *argv[]){
-	// ontiene parametos de ejecucion
+	//test pthread
+	pthread_t thread;
+
+	// Get port number
 	if (argc < 2 | argc > 2 ){
 		printf("Error: no se indico el numero de puerto\n");
 		exit(0);
@@ -59,44 +90,39 @@ int main(int argc, char const *argv[]){
 	else{
 		port = (u_short) atoi(argv[1]);
 	}
-	
-	// se cargan configruacione sy se crea el socket
-	start_server();
+	start_server(); // calls the function to setup the server
 
-	// escucha conexiones, segundo atributo maximo de conexiones
+	// prepare the socket to listen for new connections
 	printf("espreando conexion\n");
 	listen(fd,MAX_USER); 
 
 	// manejando conexiones
 	while(1) {
 		printf(" cl count %d\n", clients_count);
-		conn = accept(fd, (struct sockaddr *)&cl_socket, &cl_socket_fd);
-    	
-    	pid_t pid;
-    	pid = fork();
-    	if(pid == 0) {
-    		connected_clients[clients_count].socket = cl_socket;
-    		connected_clients[clients_count].fd = cl_socket_fd;
-    		connected_clients[clients_count].connfd = conn;
-    		clients_count ++;
-    		printf(" cl count increment %d\n", clients_count);
-	        while (recv(conn, message, 100, 0)>0) {
-	            // strcpy(message,"");
-	            printf("Message Received: %s\n", message);
-	            //An extra breaking condition can be added here (to terminate the child process)            
-	            printf("pid %d\n", pid);
-	            printf("conn fd %d\n", conn);
-	            // test
-	            int i = 0;
-	            for ( i = 0; i < MAX_USER; ++i)
-	            {
-	            	send_message(connected_clients[i].connfd, &connected_clients[i].socket);
-	            	printf("conn dentro de for %d\n",connected_clients[i].connfd);
-	            }
-	            // send_message(conn, &cl_socket);
-        	}
-        	exit(0);
-    	}	
+		conn = accept(fd, (struct sockaddr *)&cl_socket, &cl_socket_fd);	
+    	if(conn < 0){
+    		printf("error on creating a new connection\n");
+    	}
+    	inet_ntop(AF_INET, &(cl_socket.sin_addr), client_address, CLIENT_ADDRESS_LENGTH);
+    	printf("Connectado: %s\n", client_address);
+
+    	// pid_t pid;
+    	// pid = fork();
+    	//if(pid == 0) {
+		connected_clients[clients_count].socket = cl_socket;
+		connected_clients[clients_count].fd = cl_socket_fd;
+		connected_clients[clients_count].connfd = conn;
+		clients_count ++;
+		printf(" cl count increment %d\n", clients_count);
+		pthread_create(&thread, NULL, &recive, (void *) conn);
+		// printf("came back\n");
+        // while (recv(conn, message, 100, 0)>0) {
+        //     // strcpy(message,"");
+        //     printf("Message Received: %s\n", message);
+        //     //An extra breaking condition can be added here (to terminate the child process)            
+        //     // printf("pid %d\n", pid);
+        //     printf("conn fd %d\n", conn);
+    	// }
 	}
 	return 0;
 }
--- test_client.c
+++ test_client.c
@@ -4,49 +4,84 @@
 #include <netinet/in.h> //For the AF_INET (Address Family)
 #include <string.h>
 
+#define BUFFER_MSJ_SIZE 1024
+
 struct sockaddr_in server; //This is our main socket variable.
 int fd; //This is the socket file descriptor that will be used to identify the socket
 int conn; //This is the connection file descriptor that will be used to distinguish client connections.
-char message[100] = ""; //This array will store the messages that are sent by the server
-char message2[100] = "";
+char message[BUFFER_MSJ_SIZE] = ""; //This array will store the messages that are sent by the server
+char message2[BUFFER_MSJ_SIZE] = "";
 char *server_IP;
 u_short port;
 
 
+typedef struct localInfo{
+    char* userName;
+    int socket;
+}Info ;
+
+/*Recive data from server*/
+void * recive(void * threadData) {
+    int socket_fd, response;
+    char message[BUFFER_MSJ_SIZE];
+    Info* pData = (Info*)threadData;
+    socket_fd = pData->socket;
+    char* prompt = pData->userName;
+    memset(message, 0, BUFFER_MSJ_SIZE); // Clear message buffer
+
+    // Print received message
+    while(1) {
+        response = recvfrom(socket_fd, message, BUFFER_MSJ_SIZE, 0, NULL, NULL);
+        if (response == -1) {
+        	// to be implemented
+          //fprintf(stderr, "recv() failed: %s\n", strerror(errno));
+          break;
+        } else if (response == 0) {
+              printf("\nPeer disconnected\n");
+              break;
+        } else {
+              printf("\nServer> %s", message);
+              printf("%s", prompt);
+              fflush(stdout); // Make sure "User>" gets printed
+          }
+    }
+}
 
 
 int main(int argc, char const *argv[]){
+	// defina local variables
+
 	// manejo de ingreso de variables
-	if (argc < 2){
-		printf("Error: no se indico la ip y el numero de puerto\n");
-		exit(0);
-	}
-	else if(argc > 3){
-		printf("Demasiados argumentos\n");
-		exit(0);
+	if (argc > 5){
+		printf("Uso: client <username> <client_port> <server_ip> <server_port>\n");
+        exit(1);
 	}
 	else{
 		server_IP = argv[1];
 		port = (u_short) atoi(argv[2]);
 	}
-
-
-	fd = socket(AF_INET, SOCK_STREAM, 0);
-
+	fd = socket(AF_INET, SOCK_STREAM, 0); // creates new socket
+	// this should be organized
 	server.sin_family = AF_INET;
 	server.sin_port = htons(port);
 
-	inet_pton(AF_INET, server_IP, &server.sin_addr); //This binds the client to localhost
+	inet_pton(AF_INET, server_IP, &server.sin_addr); //This binds the client to the server IP
 
 	connect(fd, (struct sockaddr *)&server, sizeof(server)); //This connects the client to the server.
+	//TODO handle exceptions
 
-
-	while(1) {
+	//test
+	Info info;
+    info.userName = "you";
+    info.socket = fd;
+    pthread_t thread;
+    pthread_create(&thread, NULL, &recive, (void *) &info);
+	while(1) {	
 	    printf("Enter a message: ");
 	    fgets(message, 100, stdin);
-	    send(fd, message, strlen(message), 0);
-	    recv(fd, message2, 100, 0);
-	     printf("Message Received: %s\n", message2);
+	    send(fd, message, BUFFER_MSJ_SIZE, 0);
+	    memset(message, 0, BUFFER_MSJ_SIZE);
+	    
 	    //An extra breaking condition can be added here (to terminate the while loop)
 	}
 }
